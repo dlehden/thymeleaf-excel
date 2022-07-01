2f36327d9ae98c29ffec8cedf0340e0efbd81a56
dlehden <dlehden1@naver.com>
2022-07-01 14:24:51 +0900

    excel load

--- src/main/java/com/example/demo/controller/ThymeController.java
+++ src/main/java/com/example/demo/controller/ThymeController.java
@@ -5,11 +5,14 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.tika.Tika;
 import org.apache.tika.exception.TikaException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -93,13 +96,39 @@ public class ThymeController {
     @RequestMapping(value="/excel/read" , method=RequestMethod.POST)
    public String excelRead(@RequestParam("file1") MultipartFile file, Model model) throws TikaException, IOException { // 2
 
-        try (InputStream is = file.getInputStream();) {
+        try (InputStream is = file.getInputStream()) {
                     System.out.println("ABC");
+                    String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+                    Workbook workbook = null;
+
+                    if (extension.equals("xlsx")) {
+                      workbook = new XSSFWorkbook(file.getInputStream());
+                    } else if (extension.equals("xls")) {
+                      workbook = new HSSFWorkbook(file.getInputStream());
+                    }
+                    Sheet worksheet = workbook.getSheetAt(0);
+
+                    for (int i = 0; i < worksheet.getPhysicalNumberOfRows(); i++) {
+                        Row row = worksheet.getRow(i);
+                        
+                    
+                        System.out.println(i + "라인");
+                        System.out.println(row.getCell(0).getStringCellValue());
+                        System.out.println(row.getCell(1).getStringCellValue());
+  
+                    
+                      //ExcelData data = new ExcelData();
+                  
+                      //dataList.add(data);
+                    }
+                
            
         } catch (Exception e) {
             throw new TikaException("ERROR");
         }
-         return "index";
+         return "redirect:/";
+         //return "index";
     }
+
 
 }
